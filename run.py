3386bfbe7233e9f521d296a68fc80a2f750306a8
Taku Yamagata <taku.yamagata@bristol.ac.uk>
2023-10-03 16:27:14 +0100

    added arguments and comments to serve as example

--- run.py
+++ run.py
@@ -8,10 +8,17 @@ import numpy as np
 import main_oracle
 
 main_oracle.main(
-         algID   = 'tabQL_Cest_em_t2',
-         simInfo='_C=p8_Tr8_L=p2', # Filename header
+         algID   = 'tabQL_Cest_vi_t2',  # 'tabQL_Cest_em_org_t1', 'tabQL_Cest_em_org_t2'
+                                        # 'tabQL_Cest_em_t1', 'tabQL_Cest_em_t2'
+                                        # 'tabQL_Cest_vi_t1', 'tabQL_Cest_vi_t2'
+         simInfo='_C=p2p9_Tr15_L=p01_a=100_b=100', # Filename header
+         env_size = 'small',         # environment size ('small' or 'medium')
          trial_count = 100,          # number of learning trial
          episode_count = 2000,       # number of episodes for a single learning trial
-         L  = np.array([0.2]),     # probability to give a feedback
-         C  = np.array([0.8])      # Human feedback confidence level)
+         L  = np.ones(15) * 1e-2,    # probability to give a feedback
+         C  = np.array([0.9,  0.9,  0.8,  0.8,  0.7,  0.7,  0.6,  0.6,  0.5,  0.4,  0.4,  0.3,  0.3,  0.2,  0.2]),      # Human feedback confidence level)
+         a  = 100,
+         b  = 100,
+         no_reward = False,          # True: learns without reward
+         C_fixed = None,             # np.array(num_trainers): fix C value, None: learns C value
          )
