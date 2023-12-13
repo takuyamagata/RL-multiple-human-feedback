9093c8c9f5c806718836eb998854491c0ea9539b
JE <nl21501@bristol.ac.uk>
2023-12-13 15:02:41 +0000

    added windows for feedback

--- results/plot_result.py
+++ results/plot_result.py
@@ -4,8 +4,10 @@ Plot simulation results
 import os, glob
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.interpolate import make_interp_spline, UnivariateSpline, splrep, BSpline
 
-def plot_std(ax, fname, color=None):
+def plot_std(ax, fname, color=None,master=False):
     data = np.load(fname)
     x = np.arange(data['ave'].shape[1]) + 1
     num_data = data['ave'].shape[0]
@@ -14,8 +16,9 @@ def plot_std(ax, fname, color=None):
         if color is None:
             col = f'C{n}'
         else:
-            col = color
-        ax.plot(x, data['ave'][n], color=col)
+            col = color 
+
+        resline = ax.plot(x, data['ave'][n], color=col,label=fname)
         ax.fill_between(x, data['ave'][n]-data['std'][n], data['ave'][n]+data['std'][n], color=col, alpha=0.2)
 
     ax.set_xlabel('number of episodes')
@@ -25,7 +28,24 @@ def plot_std(ax, fname, color=None):
     else:
         ax.set_ylabel('estimated consistency level')
         ax.set_ylim(0, 1)
+    
+    if master:
+        ax.legend(loc='best',fontsize=7)
+        resline[0].remove()
+        # Increase the number of points for smoother interpolation
+        x_new = np.linspace(min(x), max(x), 200)
+
+        # Apply spline interpolation to smooth the line
+        # spl = make_interp_spline(x, data['ave'][n], k=0)
+        # y_smooth = spl(x_new)
+        tck = splrep(x, data['ave'][n], s=200)
+        y_smooth = BSpline(*tck)(x_new)
+        resline = ax.plot(x_new, y_smooth, color=col,label=fname)
+        # resline[0].set_alpha(0.1)
         
+    else:
+        ax.legend()
+        ax.get_legend().remove()
     return True
 
 def plot_pct(ax, fname, pct=50, color='C0'):
@@ -47,15 +67,18 @@ def plot_pct(ax, fname, pct=50, color='C0'):
     return True
 
 
+m_fig,m_ax = plt.subplots(1)
 
-files = glob.glob('./results/aveRW*npz')
-for fname in files:
+files = glob.glob('./results/aveRW*window.npz')
+colours = sns.color_palette("Set1",len(files))
+for i,fname in enumerate(files):
     fig, ax = plt.subplots(1)
     plot_std(ax, fname)
-
+    plot_std(m_ax, fname,color=colours[i],master=True)
     fig_fname, ext = os.path.splitext(fname)
     fig.savefig(fig_fname+'.pdf')
-    
+
+m_fig.savefig('./results/all_results.pdf')
 
 # fname = "results/aveRW_tabQL_Cest_em_org_t1_C=p2p9_Tr8_L=p2.npz"
 # fig, ax = plt.subplots(1)
--- run.py
+++ run.py
@@ -7,18 +7,22 @@ Created on Mon Feb 26 18:33:08 2018
 import numpy as np
 import main_oracle
 
-main_oracle.main(
-         algID   = 'tabQL_Cest_vi_t2',  # 'tabQL_Cest_em_org_t1', 'tabQL_Cest_em_org_t2'
+windows = [[0,400],[400,800],[800,1200],[1200,1600],[1600,2000]]
+
+for window in windows:
+    main_oracle.main(
+         algID   = 'tabQL_Cest_vi_t1',  # 'tabQL_Cest_em_org_t1', 'tabQL_Cest_em_org_t2'
                                         # 'tabQL_Cest_em_t1', 'tabQL_Cest_em_t2'
                                         # 'tabQL_Cest_vi_t1', 'tabQL_Cest_vi_t2'
-         simInfo='_C=p2p9_Tr15_L=p01_a=100_b=100', # Filename header
+         simInfo='_C=p2p9_Tr10_L=p01_a=100_b=100_' + str(window[0]) + '_' + str(window[1]) + '_' + 'window', # Filename header
          env_size = 'small',         # environment size ('small' or 'medium')
-         trial_count = 5,          # number of learning trial
-         episode_count = 2000,       # number of episodes for a single learning trial
+         trial_count = 10,          # number of learning trial
+         episode_count = 1000,       # number of episodes for a single learning trial
          L  = np.ones(15) * 1e-2,    # probability to give a feedback
-         C  = np.array([0.9,  0.9,  0.8,  0.8,  0.7,  0.7,  0.6,  0.6,  0.5,  0.4,  0.4,  0.3,  0.3,  0.2,  0.2]),      # Human feedback confidence level)
+         C  = np.array([0.9,  0.9,  0.8,  0.8,  0.7,  0.7,  0.6,  0.6,  0.5,  0.4,  0.4,  0.3,  0.3,  0.2,  0.2]),      # Human feedback consistency level)
          a  = 100,
          b  = 100,
          no_reward = False,          # True: learns without reward
          C_fixed = None,             # np.array(num_trainers): fix C value, None: learns C value
+         window = window
          )
