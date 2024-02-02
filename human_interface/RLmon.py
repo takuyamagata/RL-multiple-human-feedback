8961b08377c442fe4bafc497ed1d2e832ff97eac
Jonathan Erskine <38688728+jmerskine1@users.noreply.github.com>
2024-02-02 10:47:52 +0000

    Added module for user experiments

---
+++ human_interface/RLmon.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+ RL monitor class
+ Monitor RL results 
+
+ Note:  
+"""
+import numpy as np
+
+"""
+Episodic RL monitor
+"""
+class RLmon(object):
+    
+    def __init__(self, episode_count, numData=1):
+        self.episode_count = episode_count
+        self.numData = numData
+        # prepare a buffer for averaging reward
+        self.ave   = np.zeros([numData, episode_count])
+        self.aveSq = np.zeros([numData, episode_count])
+        return
+    
+    def store(self, episode_idx, trial_idx, d):
+        # update averaged reward        
+        self.ave[:,episode_idx]   = (self.ave[:,episode_idx]   * trial_idx + d) / (trial_idx + 1)
+        self.aveSq[:,episode_idx] = (self.aveSq[:,episode_idx] * trial_idx + d**2) / (trial_idx + 1)
+        return
+    
+    def saveData(self, fname):
+        stddev = np.sqrt( self.aveSq - self.ave**2 )
+        np.savez(fname, ave=self.ave, std=stddev)        
+        return
